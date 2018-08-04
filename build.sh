6914385d13b0ce3ee1dd3831c6fa08be7e53eae3
Jon Hopkins <jhopkins720@gmail.com>
2018-08-04 07:58:39 -0400

    Use variables for s3 path

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-aws s3 cp . s3://jonhopkins.net/old-projects --recursive  --exclude "*" --include "*.html" --include "*.js"
+source .env
+
+aws s3 cp . "s3://$S3_BUCKET/$S3_PATH" --recursive  --exclude "*" --include "*.html" --include "*.js"
